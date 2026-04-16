1a173100110131560184a2e034fc77750f5c26a7
Kuldeep Sharma <kuldeep.sharma@saucelab.com>
2026-04-16 12:20:22 +0530

    updated nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.autowrite = true
+
+-- Use blank space instead of diagonal hatching for diff filler lines
+vim.opt.fillchars:append({ diff = " " })
+
+
--- .config/nvim/lua/plugins/claudecode.lua
+++ .config/nvim/lua/plugins/claudecode.lua
@@ -2,5 +2,10 @@ return {
   {
     "coder/claudecode.nvim",
     lazy = false,
+    opts = {
+      diff_opts = {
+        open_in_new_tab = true,
+      },
+    },
   },
 }
